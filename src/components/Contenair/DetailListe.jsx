fb3a7c876a282b2f2abb1ad85bcdc92f4b12c1eb
thesynyembo <nyembothesy@gmail.com>
2021-02-23 17:40:12 +0100

    first commit

--- src/components/Contenair/DetailListe.jsx
+++ src/components/Contenair/DetailListe.jsx
@@ -47,29 +47,27 @@ margin-right:10%;
 `
 
 export default function DetailListe(changementDisplay) {
-  const [listeHopital, setListeHopital] = useState([]);
+
+  const [hopital, setHopital] = useState([]);
   const [listeSpecialite, setListeSpecialite] = useState([]);
-  // let {id} = useParams();
-  // const params = window.location.href;
-  // const id=params.split('DetailListe/');
-  // console.log("je suis params",id[1])
+
   let { id } = useParams();
   const params = { id };
   console.log(params.id);
+
   useEffect(() => {
-    axios
-      // .get(`http://localhost:5000/Map/`+id[1])
-      .get(`http://localhost:5000/Map/${params.id}?`)
-      .then((res) => {
-        setListeHopital(res.data);
-        console.log('DetailListe',res.data);
-      })
-      .catch((erreur) => console.log(erreur));
+    // axios
+    //   .get(`http://localhost:5000/Map/${params.id}?`)
+    //   .then((res) => {
+    //     setListeHopital(res.data);
+    //     console.log('DetailListe',res.data);
+    //   })
+    //   .catch((erreur) => console.log(erreur));
 
     axios
       .get(`http://localhost:5000/Map/Specialites/${params.id}?`)
       .then((res) => {
-        setListeSpecialite(res.data);
+        setHopital(res.data);
         console.log('listeSpecialite thesiaaa',res.data);
       })
       .catch((erreur) => console.log(erreur));
@@ -83,19 +81,15 @@ return (
     
       <Grid>
         <Grid.Column mobile={16} tablet={16} computer={16}> 
-          <Grid columns={1} >   
-          {
-      listeHopital.map((e,index)=>          
-          <p className='title' key={index}>{e.name}</p>
-          )}
+          <Grid columns={1} >          
+            <p className='title'>{hopital.name}</p>
           </Grid>
         </Grid.Column>
 
         <Grid.Column mobile={16} tablet={11} computer={11}>
-          {
-      listeHopital.map((e)=>
-              <Image src={e.images} style={{height: "300px", width:"100%",objectFit:"cover"}}/>
-            )}
+ 
+              <Image src={hopital.images} style={{height: "300px", width:"100%",objectFit:"cover"}}/>
+
 
         </Grid.Column>
 
@@ -109,44 +103,37 @@ return (
           </Grid>
           </Grid.Column>
 
-        {
-      listeHopital.map((e)=>
             <Grid.Column mobile={16} tablet={8} computer={8}>
               <p className='content'>
-                {e.description} 
+                {hopital.description} 
               </p>
-            </Grid.Column>  
-            
-      )}
+            </Grid.Column> 
 
-            {
-      listeHopital.map((e)=>
       <Grid.Column mobile={16} tablet={8} computer={8}>
   <>      
               <p className='content'>
                 <Icon name='map marker alternate' className="icone"/>
-                  <span className='NewsContent'>Adresse :</span> {e.adress}
+                  <span className='NewsContent'>Adresse :</span> {hopital.adress}
               </p> 
               <p className='content'><a href='tel:{e.phone}'>
                 <Icon name='phone' className="icone"/></a>
-                <span className='NewsContent'>Téléphone : </span>{e.phone} 
+                <span className='NewsContent'>Téléphone : </span>{hopital.phone} 
               </p>
               <p className='content'><a href='mailto:{e.name}'>
                 <Icon name='envelope' className="icone"/></a>
-                  <span className='NewsContent'>Gmail : </span>{e.email} 
+                  <span className='NewsContent'>Gmail : </span>{hopital.email} 
               </p>
               <p className='content'>
                 <Icon name='clock outline' className="icone"/>
-                  <span className='NewsContent'>Heure : </span>{e.heure_de_service}
+                  <span className='NewsContent'>Heure : </span>{hopital.heure_de_service}
               </p>
               <p className='content'>
                 <Icon name='info' className="icone"/>
-                  <span className='NewsContent'>Langue(s) : </span>{e.email}
+                  <span className='NewsContent'>Langue(s) : </span>{hopital.email}
               </p>
 
               </>
           </Grid.Column>
-              )}
 
 <Grid.Column mobile={16} tablet={16} computer={16}>
           <Grid columns={1} >
@@ -154,10 +141,13 @@ return (
           </Grid> 
           <Grid columns={1}>
             <Grid.Column> 
-              {
-                listeSpecialite.map((e)=>
+              { hopital.specialite == undefined 
+              ? 
+              null
+              :
+               hopital.specialite.map((e)=>
                     <ul className='content'>
-                      <li>{e.nom}</li>
+                      <li>{e.nom_specialite}</li>
                     </ul>
                     )}
             </Grid.Column>
@@ -167,14 +157,12 @@ return (
           </Grid>
           <Grid columns={1}>
             <Grid.Column> 
-              {
-                listeHopital.map((e)=>
+
                 <>
                   <p className='content'>
-                    <span>Directeur : </span>{e.directeur}
+                    <span>Directeur : </span>{hopital.directeur}
                   </p>
                 </>
-                    )}
             </Grid.Column>
           </Grid>
           </Grid.Column>
